31fd2e759834120552b5e9cd5b680aa44dfd0a85
r.harkes <r.harkes@nki.nl>
2024-10-11 16:29:43 +0200

    first commit

---
+++ DataStorageLease.py
@@ -0,0 +1,55 @@
+import tomllib
+from pathlib import Path
+import os
+import logging
+from datetime import date
+import win32com.client as com
+
+
+def main() -> None:
+    logger = logging.getLogger("DataStorageLease")
+    pth = Path(Path.cwd(), 'DataStorageLease.toml')
+    if not pth.exists():
+        logger.error("No DataStorageLease.toml file was found.")
+        return
+    with open(pth, 'rb') as fp:
+        data = tomllib.load(fp)
+
+    if data['PC'] != os.environ['COMPUTERNAME']:
+        logger.error(f"Storage file for {data['PC']} will not run on {os.environ['COMPUTERNAME']}")
+        return
+
+    folders = [x for x in Path.cwd().glob('*') if x.is_dir()]
+    for folder in folders:
+        if folder.name not in data.keys():
+            logger.warning(f"There is no lease for {folder.name}.")
+        else:
+            if data[folder.name]['untill'] < date.today():
+                logger.warning(f"Data lease for {folder.name} has expired.")
+            else:
+                foldersize = sizeoffolder(folder)
+                bytesallowed = converttobytes(data[folder.name]['amount'])
+                if foldersize > bytesallowed:
+                    logger.warning(f"{folder.name} has exceeded the allowed storage.")
+                else:
+                    logger.info(f"{folder.name} ok")
+
+
+
+def sizeoffolder(pth: Path) -> int:
+    fso = com.Dispatch("Scripting.FileSystemObject")
+    folder = fso.GetFolder(str(pth))
+    return folder.Size
+    # return sum(f.stat().st_size for f in pth.glob('**/*') if f.is_file())
+
+
+def converttobytes(text: str) -> int:
+    strfind = ['kb', 'mb', 'gb', 'tb', 'eb']
+    unit = [i for i, x in enumerate(strfind) if x in text.casefold()]
+    return int(text[0:-2]) * 10 ** ((1 + unit[0]) * 3)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
+    os.system('pause')
